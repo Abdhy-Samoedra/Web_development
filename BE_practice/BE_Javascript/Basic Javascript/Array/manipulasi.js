cde096540696a8d0b51bfde56a05dc47761d5c73
abdhy <abdhysamoedra030@gmail.com>
2023-01-01 21:23:55 +0700

    update

--- BE_practice/BE_Javascript/Basic Javascript/Array/manipulasi.js
+++ BE_practice/BE_Javascript/Basic Javascript/Array/manipulasi.js
@@ -32,7 +32,74 @@
 // console.log(arr.join(' '));
 // a. pop: untuk  menghapus elemen array diawal indexnya,menghilangkan elemen yang ada diawal arraynya
 // contoh :
-var arr = ['samu',3,4,"samudra",6];
-arr.shift();
-arr.shift();
-console.log(arr.join(' '));
+// var arr = ['samu',3,4,"samudra",6];
+// arr.shift();
+// arr.shift();
+// console.log(arr.join(' '));
+// 4. slice and splice
+// a. splice : untuk memasukkan elemen array ditengah tengah elemen
+// rumus : 
+// splice(indexAwal(mau memasukkan elemen yg berada disebeelah elemen apa), mauDihapusBerapa, elemen baru1,elemen baru 2,dst...);
+// var arr = [1,2,3,4,5,6];
+// var arr2 = [1,2,3,4,5,6];
+// arr.splice(3,0,88,99,11);
+// arr2.splice(2,2,66,77,54);
+// console.log(arr);
+// console.log(arr2);
+// b. slice : untuk mengiris sebuah array menjadi array yang baru
+// rumus
+// slice(index awal,index akhir);
+// note : index awal akan terbawa di array baru tapi index akhir tidak
+// var arr = [1,2,3,4,5,6];
+// var arr2 = arr.slice(0,2);
+// console.log(arr);
+// console.log(arr2)
+// 5. foreach dan map
+// a. forEeach : hampir sama seperti for l0op tapi lebih singkat
+// misal :
+// var angka = [1,2,3,4,5,6,7,8];
+// // jika for loop biasa :
+// for (let i = 0; i < angka.length; i++) {
+//     console.log(angka[i]);
+    
+// }
+// // jika pakai forEach:
+// angka.forEach(function (e) {
+//     console.log(e);
+// });
+// b. map : sama seperti forEach namun lebih baik karena mengembalikan nilai array
+// misal : 
+// var angka = [1,2,3,4,5,6,7,8];
+// var angka2 = angka.map(function(e) {
+//     return e*2;
+// })
+// console.log(angka2.join('-'));
+// hal ini bisa dilakukan karena map mengembalikan nilai array yang dituju sedngkan foreach tidak mengembalikan nilai
+// 6. sort : untuk mengurutkan Array
+// var angka = [4,6,8,2,4,7,0,4,2];
+// console.log(angka);
+// angka.sort();
+// console.log(angka);
+// // pada fungsi sort ini akan diurutkan berdasarkan karakter pertama, misal
+// var angka = [4,6,10,20,60,8,2,4,7,0,4,2];
+// console.log(angka);
+// // angka.sort();
+// // agar urut secara benar maka menjadi :
+// angka.sort(function(a,b){
+//     return a-b;
+// });
+// console.log(angka);
+
+// // sort array string
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits);
+// fruits.sort();
+// console.log(fruits);
+// 7. filter and find : find(mengembalikan 1 nilai),filter(mengembalikan banyak nilai)
+// a. filter : semacam search yang bisa mengembalikan banyak nilai, contoh:
+var angka = [4,6,10,20,60,8,2,4,7,0,4,2];
+var angka2 = angka.filter(function (a) {
+    // return a == 7;
+    return a >=4
+})
+console.log(angka2);
